2bcb8ebc4c194c3725235601fe64c0f86d9d4b5d
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-05-17 08:34:18 -0300

    css da página de login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import CampoTexto from "./componentes/CampoTexto";
+import Logo from "./componentes/Logo";
+
+function App() {
+  return (
+    <div className="App">
+     <Logo />
+    </div>
+  );
+}
+
+export default App;
