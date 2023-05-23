e766e91c452c50100358b40d2988422726c1cee1
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-05-23 18:55:24 -0300

    redirecionamento login para página atendimento

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,9 +2,18 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value)
+  }
+
   return (
      <div className="campo-texto">
-      <input placeholder={props.placeholder}/>
+      <input 
+      placeholder={props.placeholder}
+      value={props.valor}
+      onChange={aoDigitado}
+      required={props.obrigatorio}
+      />
     </div>
   )
 }
