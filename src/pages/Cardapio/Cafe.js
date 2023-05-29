52b5b6418bebd2ee6c018d5e957753f83ff28542
osikmelina <melina.mk@gmail.com>
2023-05-29 15:02:42 -0300

    começando a puxar os produtos pela API

--- src/pages/Cardapio/Cafe.js
+++ src/pages/Cardapio/Cafe.js
@@ -1,9 +1,19 @@
 import CaixaFundo from "../../componentes/CaixaFundo"
 import Tag from "../../componentes/Tag"
 import styles from "./Cardapio.module.css"
-
+import { useEffect, useState } from "react";
+import produtos from "../../API/products";
 
 const Cafe = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const token = localStorage.getItem("token")
+      const response = await produtos(token)
+      const listaProdutos = await response.json()
+      console.log(listaProdutos)
+    }
+    fetchData()
+  }) 
   return (
     <CaixaFundo>
       <h3 className={styles.txtItens}>CAFÉS</h3>
