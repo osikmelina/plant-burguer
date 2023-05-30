6a270dfd0e2c4c6ca4a39a2c6b1a45b543f57a15
osikmelina <melina.mk@gmail.com>
2023-05-30 18:14:17 -0300

    criando contexto para pegar nome do cliente

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,31 @@
-import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormLogin from "./pages/Login";
 import NovoPedido from "./pages/Atendimento";
 import Modal from 'react-modal';
 import Cardapio from "./pages/Cardapio";
 import { clienteContext } from "./context/clienteContext";
+import { useState } from "react";
 
 // Código necessário para os recursos de acessibilidade
 Modal.setAppElement('#root');
+
+const Store = ({children}) => {
+  const [cliente, setCliente] = useState('');
+
+  return (
+      <clienteContext.Provider value={cliente}>
+          {children}
+      </clienteContext.Provider>
+  )
+};
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FormLogin />}></Route>
-        <clienteContext.Provider>        
-        <Route path="/atendimento" element={<NovoPedido />}></Route>
-        <Route path="/cardapio" element={<Cardapio />}></Route>
-        </clienteContext.Provider>
+        <Route path="/" element={<FormLogin />}></Route>       
+        <Route path="/atendimento" element={<Store> <NovoPedido /> </Store> }></Route>
+        <Route path="/cardapio" element={<Store> <Cardapio /> </Store>}></Route>
       </Routes>
     </BrowserRouter>
   )
